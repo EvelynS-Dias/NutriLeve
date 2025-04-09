2dd2f2decee9eadb6a7cbbb7f8936e30feeeaa0d
EvelynS-Dias <Evydesouza23150@gmail.com>
2025-04-09 19:44:17 -0300

    :sparkle: feat: adiconamento de receitas

---
+++ model/receitas.js
@@ -0,0 +1,237 @@
+export const receitas = [
+    [
+        {
+          "id": 1,
+          "nome": "Bolo de Maçã com Banana e Canela",
+          "rendimento": "1 bolo médio",
+          "ingredientes": [
+            "2 maçãs descascadas, sem sementes e picadas",
+            "1 banana-nanica descascada e cortada em rodelas",
+            "2 ovos",
+            "1 xícara de chá de farelo de aveia",
+            "1 xícara de chá de farinha de trigo integral",
+            "1 colher de sopa de canela em pó",
+            "1 colher de sopa de fermento químico em pó",
+            "1 maçã fatiada e sem sementes para decorar",
+            "Canela em pó para decorar",
+            "Óleo de coco para untar",
+            "Farinha de aveia para enfarinhar"
+          ],
+          "modo_preparo": [
+            "Bata no liquidificador as maçãs, a banana e os ovos até obter uma mistura homogênea.",
+            "Transfira para um recipiente e adicione o farelo de aveia, a farinha, a canela e o fermento.",
+            "Mexa bem até incorporar tudo.",
+            "Unte uma assadeira com óleo de coco e enfarinhe com farinha de aveia.",
+            "Coloque as fatias de maçã no fundo da assadeira e despeje a massa por cima.",
+            "Asse em forno preaquecido a 180ºC até dourar.",
+            "Retire do forno, espere esfriar, desenforme e decore com canela em pó."
+          ],
+          "categoria": ["bolo saudável", "sem açúcar refinado", "rico em fibras"],
+          "nivel": "fácil",
+          "tempo_preparo": 45,
+          "recomendada_para": ["diabetes", "lanches saudáveis", "café da manhã"]
+        },
+        {
+          "id": 2,
+          "nome": "Maminha ao Molho de Ervas (com creme vegetal)",
+          "rendimento": "10 porções",
+          "ingredientes": [
+            "1 maminha em peça",
+            "1 colher de chá de sal",
+            "2 dentes de alho descascados e amassados",
+            "1 colher de sopa de azeite",
+            "2 colheres de sopa de manjericão picado",
+            "2 colheres de sopa de salsa picada",
+            "2 colheres de sopa de tomilho",
+            "1 xícara de chá de caldo de legumes",
+            "1 colher de sobremesa de amido de milho",
+            "200 g de creme de leite vegetal",
+            "Água"
+          ],
+          "modo_preparo": [
+            "Tempere a maminha com sal e alho.",
+            "Doure os dois lados da carne em azeite numa panela de pressão.",
+            "Cubra com água, tampe e cozinhe por 20 minutos após pegar pressão.",
+            "Retire a pressão, abra a panela e fatie a carne.",
+            "Bata no liquidificador as ervas, o caldo de legumes e o amido.",
+            "Transfira para uma panela, junte o creme de leite vegetal e misture bem.",
+            "Aqueça até engrossar levemente e regue a carne com o molho antes de servir."
+          ],
+          "categoria": ["proteína magra", "prato principal"],
+          "nivel": "médio",
+          "tempo_preparo": 45,
+          "recomendada_para": ["dieta equilibrada", "diabetes", "restrição à lactose"]
+        },
+        {
+          "id": 3,
+          "nome": "Musse de Maracujá com Iogurte",
+          "rendimento": "4 porções",
+          "ingredientes": [
+            "Polpa de 1 maracujá",
+            "1 xícara de chá de iogurte natural desnatado",
+            "1/2 xícara de chá de leite de amêndoas",
+            "1 colher de sopa de xilitol",
+            "10 g de gelatina incolor sem sabor",
+            "1/4 de xícara de chá de água quente",
+            "Sementes de maracujá para decorar"
+          ],
+          "modo_preparo": [
+            "Bata no liquidificador a polpa de maracujá, o iogurte, o leite de amêndoas e o xilitol.",
+            "Dissolva a gelatina na água quente e adicione à mistura no liquidificador.",
+            "Bata por 1 minuto até ficar bem homogêneo.",
+            "Distribua em taças, decore com as sementes de maracujá e leve à geladeira por 2 horas.",
+            "Sirva gelado."
+          ],
+          "categoria": ["sobremesa", "sem açúcar", "baixo carboidrato"],
+          "nivel": "fácil",
+          "tempo_preparo": 15,
+          "recomendada_para": ["diabetes", "sobremesa leve", "intolerância à lactose"]
+        },
+        {
+            "id": 4,
+            "nome": "Bolinho Integral de Banana e Passas sem Açúcar",
+            "rendimento": "10 a 12 mini bolinhos",
+            "ingredientes": [
+              "1 banana-nanica madura, amassada",
+              "1 ovo",
+              "½ xícara (chá) de suco de maçã integral sem açúcar",
+              "1 xícara (chá) de aveia em flocos finos",
+              "½ xícara (chá) de farinha de trigo integral",
+              "2 colheres (sopa) de óleo",
+              "1 colher (chá) de fermento em pó",
+              "½ xícara (chá) de uvas-passas pretas"
+            ],
+            "modo_preparo": [
+              "Em um recipiente, misture todos os ingredientes até formar uma massa homogênea.",
+              "Despeje a massa em forminhas de mini muffin (aprox. 3 cm de diâmetro).",
+              "Leve ao forno médio (180 °C), preaquecido, por cerca de 25 minutos ou até dourar levemente.",
+              "Retire do forno, espere esfriar e sirva."
+            ],
+            "categoria": ["lanche saudável", "sem açúcar", "integral"],
+            "nivel": "fácil",
+            "tempo_preparo": 30,
+            "recomendada_para": ["diabetes", "lanches infantis", "dieta sem açúcar refinado"]
+          },
+          {
+            "id": 5,
+            "nome": "Bolo Integral de Banana e Blueberry (sem Açúcar e sem Lactose)",
+            "rendimento": "1 bolo médio (forma 21x21 cm)",
+            "ingredientes": [
+              "4 bananas nanicas maduras amassadas",
+              "2 ovos",
+              "2 xícaras (chá) de aveia em flocos finos",
+              "½ xícara (chá) de farinha de trigo integral",
+              "½ colher (chá) de canela em pó",
+              "1 xícara (chá) de suco integral de maçã sem açúcar",
+              "4 colheres (sopa) de óleo",
+              "1 xícara (chá) de blueberry (mirtilo) fresco ou congelado",
+              "4 colheres (sopa) de amêndoas em lascas levemente tostadas",
+              "1 colher (sopa) de fermento em pó",
+              "½ colher (chá) de bicarbonato de sódio"
+            ],
+            "modo_preparo": [
+              "Em um recipiente, misture as bananas amassadas, os ovos, a aveia, a farinha e a canela.",
+              "Acrescente o suco de maçã, o óleo, as blueberries e as amêndoas. Misture bem.",
+              "Adicione o fermento e o bicarbonato e incorpore à massa.",
+              "Despeje a massa em uma forma quadrada (21 x 21 cm), untada com óleo e polvilhada com aveia.",
+              "Leve ao forno médio (180°C), preaquecido, por cerca de 40 minutos ou até o palito sair limpo ao espetar o centro do bolo."
+            ],
+            "categoria": ["bolo saudável", "sem açúcar", "sem lactose", "integral"],
+            "nivel": "médio",
+            "tempo_preparo": 50,
+            "recomendada_para": ["diabetes", "intolerância à lactose", "dieta sem açúcar refinado"]
+          },
+          {
+            "id": 6,
+            "nome": "Panquequinhas de Banana e Coco (Sem Açúcar e Sem Glúten)",
+            "rendimento": "8 a 10 panquequinhas (dependendo do tamanho)",
+            "ingredientes": [
+              "1/2 xícara (chá) de leite líquido MOLICO® Zero Lactose",
+              "1/2 colher (chá) de suco de limão",
+              "2 bananas-nanicas grandes e maduras",
+              "1/2 colher (chá) de essência de baunilha",
+              "1/2 xícara (chá) de farinha de arroz",
+              "4 colheres (sopa) de coco seco ralado",
+              "2 colheres (sopa) de amido de milho",
+              "1 colher (chá) de fermento em pó",
+              "1 pitada de canela em pó",
+              "Frutas variadas fatiadas (para montagem)",
+              "Azeite para untar"
+            ],
+            "modo_preparo": [
+              "Misture o leite e o suco de limão. Reserve.",
+              "Amasse as bananas em um recipiente com um garfo.",
+              "Adicione às bananas a essência de baunilha e o leite reservado. Misture bem.",
+              "Misture os ingredientes secos separadamente e, em seguida, incorpore a mistura de banana.",
+              "Aqueça uma frigideira untada com azeite e coloque colheradas da massa, mantendo espaço entre elas.",
+              "Cozinhe em fogo baixo por cerca de 4 minutos de cada lado, até dourar.",
+              "Corte as panquequinhas com um cortador redondo de 3 cm.",
+              "Monte empilhando panquequinhas intercaladas com frutas e espete com palitos para servir."
+            ],
+            "categoria": ["lanche saudável", "sem açúcar", "sem glúten", "sem lactose"],
+            "nivel": "fácil",
+            "tempo_preparo": 35,
+            "recomendada_para": ["diabetes", "intolerância à lactose", "intolerância ao glúten", "lanches infantis"]
+          },
+          {
+            "id": 7,
+            "nome": "Crepioca de Banana com Melado",
+            "rendimento": "1 porção",
+            "ingredientes": [
+              "2 colheres (sopa) de goma de tapioca",
+              "2 colheres (sopa) de purê de banana",
+              "1 banana em rodelas",
+              "1 colher (sopa) de melado",
+              "Canela a gosto",
+              "1 colher (sopa) de água",
+              "Sal a gosto",
+              "Leite vegetal (quantidade suficiente para misturar à goma)"
+            ],
+            "modo_preparo": [
+              "Em uma tigela, misture todos os ingredientes do recheio (banana em rodelas, melado, canela, água e sal) e reserve.",
+              "Para a massa, misture a goma de tapioca com o leite vegetal.",
+              "Despeje a mistura sobre uma frigideira aquecida.",
+              "Espere até a massa ficar consistente.",
+              "Espalhe o recheio sobre metade da massa e dobre ao meio.",
+              "Sirva em seguida."
+            ],
+            "categoria": ["café da manhã", "sem lactose", "vegetariano"],
+            "nivel": "fácil",
+            "tempo_preparo": 10,
+            "recomendada_para": ["intolerância à lactose", "almoço leve", "lanche saudável"]
+          },
+          
+          {
+            "id": 8,
+            "nome": "Pão de Queijo Vegano",
+            "rendimento": "30 a 45 unidades",
+            "ingredientes": [
+              "1 xícara de água",
+              "1/3 xícara de óleo ou azeite",
+              "1/2 colher (chá) de alho em pó",
+              "1 e 1/2 xícara de polvilho doce",
+              "300g de tofu",
+              "1 e 1/2 xícara de polvilho azedo",
+              "1 colher (sopa) de sal"
+            ],
+            "modo_preparo": [
+              "No liquidificador, bata o tofu com a água e o sal.",
+              "Em uma tigela grande, misture todos os ingredientes até obter uma massa lisinha.",
+              "Se a massa estiver seca, adicione um pouco mais de água aos poucos.",
+              "Se estiver úmida demais, acrescente mais polvilho.",
+              "Unte uma forma grande com óleo e modele bolinhas com a massa.",
+              "Disponha as bolinhas na forma sem encostar umas nas outras.",
+              "Leve ao congelador por pelo menos 4 horas.",
+              "Se não for assar todos, armazene os pães congelados em pote com tampa.",
+              "Para assar, unte uma forma com óleo e farinha (ou use forma antiaderente ou manta de silicone).",
+              "Leve ao forno preaquecido a 220°C por 25 a 35 minutos ou até dourar."
+            ],
+            "categoria": ["café da manhã", "sem lactose", "vegano", "sem glúten"],
+            "nivel": "fácil",
+            "tempo_preparo": 60,
+            "recomendada_para": ["intolerância à lactose", "alimentação vegana", "sem glúten"]
+          }
+
+           
+      ]
+    ]
