898e54c768894fb2f1ba1191d2dfd5b0a7d94ee1
EvelynS-Dias <Evydesouza23150@gmail.com>
2025-04-11 18:36:46 -0300

    :recycle: refactor: Arrumei o receitasController

--- src/controller/receitasController.js
+++ src/controller/receitasController.js
@@ -1,7 +1,6 @@
 import Receitas from "../model/receitas.js"
 
 
-
 export class ReceitasController {
 
 static getAllReceitas(req,res) {
@@ -9,19 +8,19 @@ static getAllReceitas(req,res) {
      }
 
   static getReceitasSemLactose(req,res) {
-     const semLactose =  Receitas[0].filter(receita => receita.para_intolerancia_lactose === true);
+     const semLactose =  Receitas[0].filter(receita => receita.paraIntoleranciaLactose === true);
      res.send(semLactose);
     }
    static getReceitasVegano(req,res) {
-        const vegano =  Receitas[0].filter(receita=>  receita.para_veganos === true);
+        const vegano =  Receitas[0].filter(receita=>  receita.paraVeganos === true);
         res.send(vegano);
         }
    static getReceitasDiabetes(req,res) {
-        const diabetes = Receitas[0].filter(receita => receita.para_diabéticos === true)
+        const diabetes = Receitas[0].filter(receita => receita.paraDiabeticos === true)
         res.send(diabetes);
             }
    static getReceitasSemGluten(req,res) {
-        const semGluten = Receitas[0].filter(receita => receita.sem_gluten === true)
+        const semGluten = Receitas[0].filter(receita => receita.semGluten === true)
          res.send(semGluten);
             }
 }
